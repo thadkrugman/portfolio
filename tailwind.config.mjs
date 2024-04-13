2e03aa3f9b1a99371c3307d98409fdc60a6eaf78
Thad <hello@thadkrugman.com>
2024-04-13 03:33:32 -0400

    init commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        base: '#131313',
+        'base-content': '#ffffff',
+        'base-content-dark': '#949494',
+        primary: '#3CFFD0',
+        'primary-content': '#000000',
+        'primary-accent': '#9EFFE8',
+        'primary-accent-dark': 'rgba(0, 0, 0, .3)',
+        secondary: '#5200FF',
+        'secondary-content': '#ffffff',
+        'secondary-accent-light': '#3CFFD0',
+        'secondary-accent-dark': '#4200CC',
+      },
+      maxWidth: {
+        custom: '1100px',
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '20px',
+          sm: '20px',
+          lg: '32',
+        },
+        screens: {
+          DEFAULT: '740px',
+          lg: '1164px',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
